bd70dea08b57c131d0525b06d5ea052c7a69a458
Miroslav Bendík <miroslav.bendik@gmail.com>
2018-08-02 12:45:51 +0200

    Replaced slugify filter

--- django_autoslugfield/utils.py
+++ django_autoslugfield/utils.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.template.defaultfilters import slugify
 from django.utils.encoding import force_text
+from django.utils.text import slugify
 
 
 def get_title(instance, title_field=None):
