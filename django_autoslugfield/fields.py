d539c649e8326e9789ccaaf5939939fb444fe460
Miroslav Bendík <miroslav.bendik@gmail.com>
2022-12-25 07:24:46 +0100

    refactor: Removed old python2 compatibility

--- django_autoslugfield/fields.py
+++ django_autoslugfield/fields.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db.models import signals, SlugField
 
 from .utils import unique_slugify
@@ -8,14 +6,14 @@ from .utils import unique_slugify
 
 class AutoSlugField(SlugField):
 	def __init__(self, reserve_chars=5, title_field=None, in_respect_to=(), *args, **kwargs):
-		super(AutoSlugField, self).__init__(*args, **kwargs)
+		super().__init__(*args, **kwargs)
 		self.reserve_chars = reserve_chars
 		self.title_field = title_field
 		self.in_respect_to = in_respect_to
 
 	def contribute_to_class(self, cls, name, virtual_only=False):
 		signals.pre_save.connect(self.unique_slugify, sender=cls)
-		super(AutoSlugField, self).contribute_to_class(cls, name, virtual_only)
+		super().contribute_to_class(cls, name, virtual_only)
 
 	def unique_slugify(self, instance, **kwargs): #pylint: disable=unused-argument
 		return unique_slugify(instance, self.name, self.reserve_chars, self.title_field, self.in_respect_to)
